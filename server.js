d81ea16687dceaa452089b7677f856cbf0f626bd
salonis66 <salonisharmagvd@gmail.com>
2026-05-06 21:10:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,12 +16,13 @@ import cookieParser from 'cookie-parser';
 
 import mysql from "mysql2/promise"
 
-const db=mysql.createPool({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 
 app.use(express.static("public"))
